78a613571c6336d85504193efe6326feb9dd969d
sristim <mesristi@gmail.com>
2020-11-27 09:58:36 +0545

    Added arrow,closure,etc functions

--- Functions.js
+++ Functions.js
@@ -20,3 +20,82 @@ let fun1 = function(number) {
 
 let a = fun1(12)
 console.log(a);
+
+//arrow function: uses arrow:lambda operation, there is no function name 
+//example-1
+var fun2 = number => number * number;
+
+//example-2
+(a, b) => {
+    let sum = a + b;
+    return sum;
+}
+
+//Closure
+//Example-1
+function outside(x) {
+    function inside(y) { return x + y; }
+    return inside;
+}
+
+let inside_fn = outside(10); //x:Gives a function that adds 10 to whatever argument is given
+let result = inside_fn(40); //y
+console.log(result);
+
+//Example-2
+function addSquares(a, b) {
+    function square(x) {
+        return x * x;
+    }
+    let result = square(a) + square(b);
+    return result;
+}
+
+let r = addSquares(12 * 13);
+
+//Example-3
+// let createPet = functon (name) {
+//     return {
+//         setName: function(newName) {
+//             name = newName;
+//         },
+//         getName: function() {
+//             return name;
+//         }
+//     }
+// }
+
+// let dog=createPet('diamond');
+
+//Example-4: Using arrow function
+let createPet = name => {
+    return {
+        setName: newName => name = newName,
+        getName: () => name
+    }
+}
+
+let dog = createPet('diamond');
+
+///////////////////////////////////////Parameters
+
+function formatName(salutation, name, address) {
+    let completeName = salutation + ", " + name + ", " + address;
+    return completeName;
+}
+
+let n = formatName("Ms.", "Sristi Musyaju", "ktm");
+let n1 = formatName("Sristi Musyaju", "ktm");
+
+console.log(n);
+console.log(n1);
+
+//Rest parameters
+function add(a, b, ...moreArgs) {
+    console.log(a);
+    console.log(b);
+    console.log(moreArgs);
+}
+
+add(12, 34);
+add(1, 2, 3, 4);
